3cc39bae92779a0551733ef6d4384fcbeb93a99e
Wojciech Gryglas <wgryglas@meil.pw.edu.pl>
2016-04-27 14:50:00 +0200

    Adding script for basic installation test

---
+++ test_scritp.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+array = [1, 3, 5, 7, 9]
+
+numpyArray = np.array(array)
+
+numpyArray2 = np.sin(array)
+
+
+plt.figure()
+plt.plot(numpyArray, numpyArray2, '.', markersize=20)
+plt.show()
